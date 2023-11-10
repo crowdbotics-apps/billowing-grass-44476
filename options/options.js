0c214eaba85fd3359646692406b075ec7a390aa6
Crowdbotics <team@crowdbotics.com>
2023-11-10 21:16:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-grass-44476",
+  url: "https://billowing-grass-44476.botics.co",
+  api: "https://billowing-grass-44476.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
